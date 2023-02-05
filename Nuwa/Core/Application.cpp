310227ecc257e237c69366b92c520fc3cdf68ed1
ghostyii <ghostyiigame@outlook.com>
2023-02-06 00:24:28 +0800

    init glfw

---
+++ Nuwa/Core/Application.cpp
@@ -0,0 +1,31 @@
+#include <GLFW/glfw3.h>
+
+int main(int argc, char** argv)
+{
+	GLFWwindow* window = nullptr;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(1366, 768, "Nuwa Framework", nullptr, nullptr);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
+	return 0;
+}
