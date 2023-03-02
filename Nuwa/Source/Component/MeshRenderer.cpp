513242f6ad180805c38bb1b341001d7c08e4d36c
ghostyii <ghostyiigame@outlook.com>
2023-03-03 01:29:19 +0800

    mesh renderer

--- Nuwa/Source/Component/MeshRenderer.cpp
+++ Nuwa/Source/Component/MeshRenderer.cpp
@@ -18,65 +18,7 @@ namespace Nuwa
 	{
 		GL_ASSERT(glEnable(GL_BLEND));
 		GL_ASSERT(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-
 	}
-
-	//void MeshRenderer::SetMesh(Mesh* mesh, const std::string& shaderPath)
-	//{
-	//	this->mesh = std::make_shared<Mesh>(*mesh);
-
-	//	GL_ASSERT(glEnable(GL_BLEND));
-	//	GL_ASSERT(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-	//	vao = std::make_unique<VertexArray>();
-	//	vbo = std::make_unique<VertexBuffer>(mesh->GetVertices().data(), mesh->GetVerticesSize());
-
-	//	VertexLayout layout;
-	//	layout.Push<float>(3);	//pos
-	//	layout.Push<float>(2);	//uv
-	//	layout.Push<float>(3);	//norm
-
-	//	vao->AddBuffer(*vbo, layout);
-
-	//	ibo = std::make_unique<IndexBuffer>(mesh->GetIndices().data(), mesh->GetIndicesSize());
-
-	//	// TODO: 验证文件是否存在
-	//	if (shaderPath != "")
-	//	{
-	//		material = std::make_shared<Material>(new Shader(shaderPath));
-	//		material->SetAlbedoMap("Resources/Textures/albedo.jpg");
-	//		material->SetSpecularMap("Resources/Textures/albedo.jpg");
-	//	}
-	//}
-
-	//void MeshRenderer::SetMesh(Mesh* mesh, const std::string& vertShader, const std::string& fragShader)
-	//{
-	//	this->mesh = std::make_shared<Mesh>(*mesh);
-
-	//	GL_ASSERT(glEnable(GL_BLEND));
-	//	GL_ASSERT(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-	//	vao = std::make_unique<VertexArray>();
-	//	vbo = std::make_unique<VertexBuffer>(mesh->GetVertices().data(), mesh->GetVerticesSize());
-
-	//	VertexLayout layout;
-	//	layout.Push<float>(3);	//pos
-	//	layout.Push<float>(2);	//uv
-	//	layout.Push<float>(3);	//norm
-
-	//	vao->AddBuffer(*vbo, layout);
-
-	//	ibo = std::make_unique<IndexBuffer>(mesh->GetIndices().data(), mesh->GetIndicesSize());
-
-	//	// TODO: 验证文件是否存在
-	//	if (vertShader != "" && fragShader != "")
-	//	{
-	//		material = std::make_shared<Material>(new Shader(vertShader, fragShader));
-	//		material->SetAlbedoMap("Resources/Textures/albedo.jpg");
-	//		material->SetSpecularMap("Resources/Textures/albedo.jpg");
-	//	}
-	//}
 
 	void MeshRenderer::SetMaterial(Material* material)
 	{
